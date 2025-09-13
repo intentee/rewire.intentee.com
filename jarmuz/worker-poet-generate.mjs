f19784f844b9cd20f7aeb01d74cc398cde96876c
Mateusz Charytoniuk <mateusz.charytoniuk@protonmail.com>
2025-09-13 18:44:31 +0200

    GH pages workflow

--- jarmuz/worker-poet-generate.mjs
+++ jarmuz/worker-poet-generate.mjs
@@ -3,7 +3,7 @@ import { spawner } from "jarmuz/job-types";
 spawner(function ({ baseDirectory, command }) {
   return command(`
       poet generate ${baseDirectory}
-        --public-path https://grouper.intentee.com/
+        --public-path https://rewire.intentee.com/
         --output-directory public
     `);
 });
